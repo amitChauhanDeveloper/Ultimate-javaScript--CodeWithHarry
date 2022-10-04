ca0ac9fe21248d6e5c15b7e16f1f0a8f3bf5e157
Amit <amitharidwarchauhan@gmail.com>
2022-10-04 18:42:24 +0530

    Video 11 Completed

--- 11_Function.js
+++ 11_Function.js
@@ -1,7 +1,22 @@
+const hello = () =>{
+    console.log("Hey how are you. I am fine and you ?");
+    return "I am also fine"
+}
+function onePlusAvg(x,y) {
+    return 1+(x+y) / 2
+    
+}
+
+const sum = (p,q)=>{
+    return p+q
+}
+
 let a = 1;
 let b = 2;
 let c = 3;
-
-console.log("Average of a and b is", (a+b)/2);
-console.log("Average of b and c is", (b+c)/2);
-console.log("Average of c and a is", (c+a)/2);
+let v =hello();
+console.log(v);
+console.log("One Plus Average of a and b is", onePlusAvg(a,b));
+console.log("One Plus Average of b and c is", onePlusAvg(b,c));
+console.log("One Plus Average of c and a is", onePlusAvg(c,a));
+console.log(sum(9,6));
